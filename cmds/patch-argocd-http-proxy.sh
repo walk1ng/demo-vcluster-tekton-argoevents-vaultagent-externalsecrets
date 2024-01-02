a71d391abf677e953d2f21119cc883c2707c303b
walk1ng <iliwgg@gmail.com>
2024-01-02 16:30:40 +0800

    fix api version for crb

---
+++ cmds/patch-argocd-http-proxy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+https_proxy=http://192.168.56.1:7890
+http_proxy=http://192.168.56.1:7890
+no_proxy="localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,10.244.0.0/16,10.96.0.0/12,.svc,.cluster.local"
+
+kubectl -n argocd create configmap proxy-config \
+	--from-literal http_proxy="$http_proxy" \
+	--from-literal HTTP_PROXY="$http_proxy" \
+	--from-literal https_proxy="$https_proxy" \
+	--from-literal HTTPS_PROXY="$https_proxy" \
+	--from-literal no_proxy="$no_proxy" \
+	--from-literal NO_PROXY="$no_proxy" 
+
+kubectl patch -n argocd \
+	-p '{ "spec": {"template": { "spec": { "containers": [ { "name": "argocd-server", "envFrom": [ { "configMapRef": {"name": "proxy-config" } } ] } ] } } } }' \
+       	deployment argocd-server
+
